f8c2e3a4d2345ab5b13e879fd0b12168cbd146d1
akashgiricse <akashgiricse@gmail.com>
2019-05-26 10:25:21 +0530

    Code refector

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,20 +5,10 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 	def create
 		user = User.new user_params
 		if user.save
-			render json: {
-				message: "Signed up successfully",
-				is_success: true,
-				data: {
-					user: user
-				}
-			}, status: :ok
+			json_response "Signed up successfully", true, {user: user}, :ok
 		else
-			render json: {
-				message: "Something wrong",
-				is_success: false,
-				data: {}
-				
-			}, status: :unprocessable_entry
+			json_response "Something wrong", false, {}, :unprocessable_entry
+			
 		end
 	end
 
@@ -30,11 +20,8 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
 	def ensure_params_exist
 		return if params[:user].present?
-		render json: {
-			message: "Missing params",
-				is_success: false,
-				data: {}
-		}, status: :bad_request
+		json_response "Missing params", false, {}, :bad_request
+		
 	end
 
 end
